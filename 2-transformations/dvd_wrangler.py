d6d1964757aca6af1f7e042db899fc0b68a4bbb7
luis <luis.felipe32971@gmail.com>
2024-02-27 20:13:56 -0300

    feat: new transformations code

--- 2-transformations/dvd_wrangler.py
+++ 2-transformations/dvd_wrangler.py
@@ -6,7 +6,16 @@ import pandas as pd
 class DVDWrangler:
 
     @staticmethod
-    def drop_columns(df: pd.DataFrame, columns: list):
+    def drop_columns(df: pd.DataFrame, columns: list) -> pd.DataFrame:
+        """_summary_
+
+        Args:
+            df (pd.DataFrame): _description_
+            columns (list): _description_
+
+        Returns:
+            pd.DataFrame: _description_
+        """
 
         df = df.drop(columns, axis=1)
 
